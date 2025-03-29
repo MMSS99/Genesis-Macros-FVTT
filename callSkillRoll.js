73696cba71a5fa4c4464249b4c91301fe47cd294
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-03-29 22:54:43 +0100

    chore(callSkillRoll): remove console.log calls used for debugging the macro

--- callSkillRoll.js
+++ callSkillRoll.js
@@ -1,14 +1,11 @@
 // sacamos el actor seleccionado
-console.log("n");
 const selectoractor = game.macros.get("1tnVvWQAtYaAjTdC");
 const actor = await selectoractor.execute();
-console.log(actor); 
 
 // obtenemos habilidad
 const caracteristica = actor.system.attributes.CHAR.XXX
 const habilidad = actor.system.attributes.HABXXX.XXXXXXX
 
-console.log("NNI")
 
 // lanzamos tirada
 const rollmacro = game.macros.get("FXJH1rfQ0HSJemOX");
